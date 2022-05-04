cc1e6a6ec4c699e18a93cd2d4d0555ce16f9616a
Erick Serejo <erickrainier@hotmail.com>
2022-05-04 17:36:34 -0300

    Iniciado aula 1 e 2

---
+++ src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,5 @@
+export function FeedbackSuccessStep(){
+    return (
+        <h1>Hellow2</h1>
+    )
+}
---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,63 @@
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from './../../assets/emoji.svg'
+import ideaImageUrl from './../../assets/idea.svg'
+import thoughtImageUrl from './../../assets/thought.svg'
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+    Bug: {
+        title:"Problema",
+        image:{
+            source:bugImageUrl,
+            alt:'Inseto'
+        }
+    },
+    Idea:{
+        title:"Ideia",
+        image:{
+            source:ideaImageUrl,
+            alt:'Lâmpada'
+        }
+        
+    },
+    Other: {
+        title:"Outro",
+        image:{
+            source:thoughtImageUrl,
+            alt:'Balão'
+        }
+    }
+}
+
+export type FeedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+
+    const [feedbackType, setfeedbackType] = useState<FeedbackType | null>(null)
+    
+    function handleRestartFeedback(){
+        setfeedbackType(null)
+    }
+
+    return(
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            
+
+            {!feedbackType ? (
+               <FeedbackTypeStep onFeedbackTypeChanged={setfeedbackType}/>
+            ) : (
+               <FeedbackContentStep 
+               feedbackType={feedbackType}
+               onFeedbackRestartRequested={handleRestartFeedback}
+               />
+            )}
+
+            <footer className="text-xs text-neutral-400">
+                Feito por <a className="underline underline-offset-2"href="https://github.com/Serejo">Serejo</a>
+            </footer>
+        </div>
+    )
+}
