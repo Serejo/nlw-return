70c6de6213554a59b6333ee13f04fa7e73b62fdd
Erick Serejo <erickrainier@hotmail.com>
2022-05-05 09:06:03 -0300

    Adição de pacotes e text-area

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -30,7 +30,7 @@ export function FeedbackContentStep({feedbackType, onFeedbackRestartRequested }:
             <CloseButton/>
         </header>
         <form className="my-4 w-full">
-            <textarea className="min-w-[304px] w-full min-h-[112px] text-sm placeholder:bg-zinc-400"
+            <textarea className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
             placeholder="Conte com detalhes o que está acontecendo..."
             >
             </textarea>
